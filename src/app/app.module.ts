485978abdab757464c31c378c94c4794a1cd1256
Keglev <ckbuzin1@gmail.com>
2024-09-30 11:46:56 +0200

    Added some features to the Buzzfeed Project: Added a button for retake the quiz and language selection (En De and Pt)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
 import { HomeComponent } from './pages/home/home.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 
 @NgModule({
   declarations: [
@@ -14,9 +25,17 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
+    provideHttpClient(withInterceptorsFromDi()),
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,37 +1,65 @@
 import { Component } from '@angular/core';
 
-import quizz_questions from "../../../assets/data/quizz_questions.json"
+import quizz_questions from "../../../assets/data/quizz_questions.json";
+
+import { TranslateService } from '@ngx-translate/core';
+
+type Language = 'en' | 'de' | 'pt';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
+
 export class QuizzComponent {
-  title:string=""
+  title:string="";
 
-  questions:any
-  questionSelected: any
+  questions:any;
+  questionSelected: any;
 
-  answers:string[] = []
-  answerSelected: string =""
+  answers:string[] = [];
+  answerSelected: string ="";
 
-  questionIndex:number = 0
-  questionMaxIndex:number = 0
-  finished:boolean = false
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+  finished:boolean = false;
+
+  currentLang: Language = 'en';
+
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang as Language;
+      this.loadQuiz();
+    });
+  }
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
+    this.loadQuiz();
+  }
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+  loadQuiz() {
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
+    const quizData = quizz_questions[this.currentLang as keyof typeof quizz_questions];
+
+    if(quizData){
+      this.finished = false;
+      this.title = quizData.title;
+      this.questions = quizData.questions;
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+      this.questionSelected = this.questions[this.questionIndex];
     }
   }
+
+  retakeQuiz() {
+    this.finished = false;
+    this.answers = [];
+    this.questionIndex = 0;
+    this.questionSelected = this.questions[this.questionIndex];
+  }
+
 
   playerChoose(value:string){
     this.answers.push(value)
@@ -42,21 +70,23 @@ export class QuizzComponent {
     this.questionIndex +=1
 
     if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
-      this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof
-        typeof quizz_questions.results]
+        this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer:string = await this.checkResult(this.answers);
+      this.finished = true;
+
+      const currentQuiz = quizz_questions[this.currentLang as keyof typeof quizz_questions];
+      const results = currentQuiz.results as { [key:string]: string };
+      this.answerSelected = results[finalAnswer as keyof typeof results];
       // Verify the win option
     }
   }
 
   async checkResult(answers:string[]){
-    const result = answers.reduce((previous, current, i, arr)=>{
+    const result = answers.reduce((previous, current, i, arr) => {
       if(
         arr.filter(item => item === previous).length >
-        arr.filter(item => item === previous).length
+        arr.filter(item => item === current).length
       ){
         return previous
       }else{
